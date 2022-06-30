b6a487aec45ca36de0529fad3750f93397c8e2ff
NaCesca <69862866+NaCesca@users.noreply.github.com>
2022-06-30 23:58:25 +0200

    implemented drag and drop function

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,7 @@
 import WeatherManager from "./WeatherManager.js";
 import WidgetManager from "./WidgetManager.js";
 
+
 function init() {
     console.log("### Starting Weather-App ###"); // eslint-disable-line no-console
     // TODO Wetter-App implementieren
@@ -8,8 +9,21 @@ function init() {
     for (var i = 0; i < localStorage.length; i++) {
         var key = localStorage.key(i),
             savedWidgets = new WeatherManager(localStorage.getItem(key));
-        savedWidgets.fecthWeather(true);
+        savedWidgets.fetchWeather(true);
     }
+
+//creates new Array after drag and drop
+    let widgetArray = [];
+    const widget = document.querySelector('#widget-list');
+    Sortable.create(widget, {
+        animation: 150,
+        onUpdate: function (evt) {
+        widgetArray = []; //neues Array wird gecleard
+        evt.to.childNodes.forEach(widget => { //für jedes Kindobjekt wird als widget übernommen
+            widgetArray.push(widget.innerText); //Inhalt wird in Array gepushed
+        });
+        },
+    });
 }
 
 var weatherManager,
@@ -22,11 +36,10 @@ document.querySelector(".widgets").addEventListener("keyup", function(event) {
             
         } else {
             weatherManager = new WeatherManager(document.getElementById("input-text").value);
-            weatherManager.fecthWeather(false);
+            weatherManager.fetchWeather(false);
             document.getElementById("input-text").value = "";
             console.log(localStorage);
-        }
-        
+        } 
     }
 })
 
